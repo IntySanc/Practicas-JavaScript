ffc0f032598fb8f28d3585edee2560cea9e06c43
Inty <inty_sanchez.gra@hotmail.com>
2021-06-13 14:43:30 -0600

    Mis primeras practicas con JavaScript

---
+++ Variables.js
@@ -0,0 +1,16 @@
+var nombre = "inty";
+
+var edad;
+
+edad = 30;
+
+// Declarar
+
+var edad;
+
+var elementos = ["Computadoras"; "Celular"];
+
+var persona = {
+    nombre = "bayardo";
+    edad = 30
+}
